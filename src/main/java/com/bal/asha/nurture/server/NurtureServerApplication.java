78380e6a6b766bc3cc4e5e50af00a3b7541c45cb
Ish Mahajan <ishmahajanbhanu@gmail.com>
2023-11-18 00:10:46 +0530

    add Azure SQL DB

--- src/main/java/com/bal/asha/nurture/server/NurtureServerApplication.java
+++ src/main/java/com/bal/asha/nurture/server/NurtureServerApplication.java
@@ -1,9 +1,11 @@
 package com.bal.asha.nurture.server;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class NurtureServerApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/bal/asha/nurture/server/NurtureServerApplicationTests.java
+++ src/test/java/com/bal/asha/nurture/server/NurtureServerApplicationTests.java
@@ -3,10 +3,10 @@ package com.bal.asha.nurture.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NurtureServerApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
