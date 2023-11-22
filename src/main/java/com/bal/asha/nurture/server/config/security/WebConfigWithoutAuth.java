db1d15c309656e85ed3b3f9086559fffd5aafddc
Ish Mahajan <ishmahajanbhanu@gmail.com>
2023-11-22 11:15:40 +0530

    add swagger

--- src/main/java/com/bal/asha/nurture/server/config/security/WebConfigWithoutAuth.java
+++ src/main/java/com/bal/asha/nurture/server/config/security/WebConfigWithoutAuth.java
@@ -1,13 +1,13 @@
 package com.bal.asha.nurture.server.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("!AuthOn")
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfigWithoutAuth implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/bal/asha/nurture/server/config/security/WebMvcConfig.java
+++ src/main/java/com/bal/asha/nurture/server/config/security/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.bal.asha.nurture.server.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtAuthenticationInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
